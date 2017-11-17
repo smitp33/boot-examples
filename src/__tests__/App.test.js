6386d6fcd0d6d890ddbfbd6b989d6d0842bc7eda
Philip J Smith <Philip.J.Smith@aexp.com>
2017-11-17 09:51:23 +0000

    latest

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
+import ApplicationsPage from '../components/applications/ApplicationsPage'
+import NoMatch from '../components/common/NoMatch'
+import AvatarCard from '../components/common/AvatarCard'
 import {BrowserRouter} from 'react-router-dom'
+import {mount} from 'enzyme'
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -10,3 +15,35 @@ it('renders without crashing', () => {
                     </BrowserRouter>,
                     div);
 });
+
+// Stateless components
+it('NoMatch', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<NoMatch />,div);
+});
+
+it('ApplicationsPage', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ApplicationsPage />,div);
+});
+
+it('AvatarCard', () => {
+  const props = {id: 1,
+    avatar_url: 'https://avatars3.githubusercontent.com/u/12224823?v=4',
+    name: 'NAME',
+    notifications: 0}
+  const div = document.createElement('div');
+  ReactDOM.render(<AvatarCard {...props}/>,div);
+});
+
+// Functions
+it('function handSelect', () => {
+  const wrapper = mount(<BrowserRouter>
+                          <App />
+                        </BrowserRouter>)
+
+  const app = wrapper.find('App');
+  app.instance().handleSelect()
+
+});
+
